3cf57689a1063d0b9c423647764ef995a8496de7
urnamee <100465709+urnamee@users.noreply.github.com>
2022-03-24 20:19:24 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -5,8 +5,8 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="ZeeoneOfc"
-global.ownernumber = "62887435047326"
+global.ownername ="Masuzu Bot."
+global.ownernumber = "6281395374557"
 global.botname = "Haruka-Bot"
 global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
